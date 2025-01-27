53251bd561de2ec10759c183f40d74fbeb7f4288
Marco pchome <augusto.marco@gmail.com>
2025-01-26 22:59:05 -0400

    unit test customer save

--- src/main/java/com/service/catering/application/model/customer/CustomerDto.java
+++ src/main/java/com/service/catering/application/model/customer/CustomerDto.java
@@ -1,8 +1,14 @@
 package com.service.catering.application.model.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDto {
 
     private String id;
--- src/main/java/com/service/catering/domain/model/CustomerEntity.java
+++ src/main/java/com/service/catering/domain/model/CustomerEntity.java
@@ -2,10 +2,16 @@ package com.service.catering.domain.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity( name = "Customer")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerEntity {
 
     @Id
