98814b2dd580a378d86400db49c16d51a1bc17a8
Marco pchome <augusto.marco@gmail.com>
2025-05-11 13:41:42 -0400

    stage-prueba2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
-docker run -d -p 80:80 --name nutricenter-"$hostname" augustomarco/nutricenter:"$hostname"
+docker run -d -p 8090:8090 --name nutricenter-"$hostname" augustomarco/nutricenter:"$hostname"
