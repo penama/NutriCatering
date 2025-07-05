472d46189c67fb69e55050f30b39f9cd5bc00baa
Marco pchome <augusto.marco@gmail.com>
2025-07-05 03:08:14 -0400

    add baseService

--- src/main/java/com/service/catering/infraestructure/config/TelemetryConfig.java
+++ src/main/java/com/service/catering/infraestructure/config/TelemetryConfig.java
@@ -1,13 +1,13 @@
 package com.service.catering.infraestructure.config;
 
-import com.microsoft.applicationinsights.extensibility.TelemetryInitializer;
-import com.microsoft.applicationinsights.telemetry.Telemetry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.extensibility.TelemetryInitializer;
 import com.microsoft.applicationinsights.extensibility.context.CloudContext;
+import com.microsoft.applicationinsights.telemetry.Telemetry;
 import com.microsoft.applicationinsights.telemetry.TelemetryContext;
 
 @Configuration
@@ -21,18 +21,18 @@ public class TelemetryConfig {
     TelemetryClient client = new TelemetryClient();
     TelemetryContext context = client.getContext();
     CloudContext cloudContext = context.getCloud();
-//    cloudContext.setRole(rolName); // Aquí se asigna el nombre de rol
-    cloudContext.setRole( "Catering-Payment" ); // Aquí se asigna el nombre de rol
+    //    cloudContext.setRole(rolName); // Aquí se asigna el nombre de rol
+    cloudContext.setRole("Catering-Payment"); // Aquí se asigna el nombre de rol
     return client;
   }
 
-	@Bean
-	public TelemetryInitializer roleNameInitializer() {
-		return new TelemetryInitializer() {
-			@Override
-			public void initialize(Telemetry telemetry) {
-				telemetry.getContext().getCloud().setRole( "Catering-Payment");
-			}
-		};
-	}
+  @Bean
+  public TelemetryInitializer roleNameInitializer() {
+    return new TelemetryInitializer() {
+      @Override
+      public void initialize(Telemetry telemetry) {
+        telemetry.getContext().getCloud().setRole("Catering-Payment");
+      }
+    };
+  }
 }
