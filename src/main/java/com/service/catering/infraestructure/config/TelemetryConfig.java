c51decffe6d7b775c16d70d299cd25898e96746b
Marco pchome <augusto.marco@gmail.com>
2025-07-05 02:54:26 -0400

    add baseService

--- src/main/java/com/service/catering/infraestructure/config/TelemetryConfig.java
+++ src/main/java/com/service/catering/infraestructure/config/TelemetryConfig.java
@@ -1,25 +1,25 @@
 package com.service.catering.infraestructure.config;
 
-import com.microsoft.applicationinsights.extensibility.context.CloudContext;
-import com.microsoft.applicationinsights.telemetry.TelemetryContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.extensibility.context.CloudContext;
+import com.microsoft.applicationinsights.telemetry.TelemetryContext;
 
 @Configuration
 public class TelemetryConfig {
 
-	@Value("${applicationinsights.role.name}")
-	String rolName;
+  @Value("${applicationinsights.role.name}")
+  String rolName;
 
   @Bean
   public TelemetryClient telemetryClient() {
-	  TelemetryClient client = new TelemetryClient();
-	  TelemetryContext context = client.getContext();
-	  CloudContext cloudContext = context.getCloud();
-	  cloudContext.setRole( rolName );  // Aquí se asigna el nombre de rol
-	  return client;
+    TelemetryClient client = new TelemetryClient();
+    TelemetryContext context = client.getContext();
+    CloudContext cloudContext = context.getCloud();
+    cloudContext.setRole(rolName); // Aquí se asigna el nombre de rol
+    return client;
   }
 }
